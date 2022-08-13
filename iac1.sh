1bf1c4730da8beb1d175b3c6eaf9e87c341e8fdd
Thales Macêdo <thalesmacedo1@live.com>
2022-08-13 20:15:04 -0300

    specifying directories permissions commands

--- iac1.sh
+++ iac1.sh
@@ -26,3 +26,16 @@ useradd roberto -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_VEN
 useradd josefina -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_SEC
 useradd amanda -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_SEC
 useradd rogerio -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_SEC
+
+echo "Specifying directories permissions..."
+
+chown root:GRP_ADM /adm
+chown root:GRP_VEN /ven
+chown root:GRP_SEC /sec
+
+chmod 770 /adm
+chmod 770 /ven
+chmod 770 /sec
+chmod 777 /publico
+
+echo "Done..."
